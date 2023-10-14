e0a7a2ab6c09750fd63435441c020a5e2c104441
Hafiz Ahmed Ali Ansari <hafizahmedaliansari123@gmail.com>
2023-10-14 12:45:25 +0500

    Ahmed Lnh Clone Hospital
    Liaquat National Hospital is a leading medical institution in Pakistan, providing compassionate patient care, healthcare services and education.

---
+++ Liaqait National Hospital/js/script.js
@@ -0,0 +1,20 @@
+/* <!-- /Code for Navbar Icon Toggle Animation / --> */
+
+
+let searchForm = document.querySelector('.search-form');
+
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+
+let nav1 = document.querySelector('.dropbtn');
+
+
+document.querySelector('#my_nav').onhover = () => {
+    nav1.classList.toggle('active');
+}
+
+
+/* <!-- /Code for Navbar Icon Toggle Animation / --> */
